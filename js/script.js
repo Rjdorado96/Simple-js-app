f2c48a8c14cb432ba7ce7a0241c0308e6209018e
Rjdorado96 <rjdorado96@outlook.com>
2022-10-25 12:21:10 -0500

    Update script.js
    Added an Array with three objects that each contain a pokemon and their characteristics.

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+    {name: 'Machamp', height: 1.6, type: 'fighting'},
+    {name: 'Primeape', height: 1, type: 'fighting'},
+    {name: 'Infernape', height: 1.2, type: ['fire', 'fighting']}
+];
