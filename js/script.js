0d98dad247bcb1560bda2fd34397608b2553988c
Rjdorado96 <rjdorado96@outlook.com>
2022-11-02 11:57:18 -0500

    implemented IIFE to script.js
    wrapped pokemonList in IIFE to limit access to the array. Updated the forEach() code to compensate for the change.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
-let pokemonList = [
+/*let pokemonList = [
     {name: 'Machamp', height: 1.6, type: 'fighting'},
     {name: 'Primeape', height: 1, type: 'fighting'},
     {name: 'Infernape', height: 1.2, type: ['fire', 'fighting']}
-];
+];*/
+
+let pokemonRepository = (function (){
+    let pokemonList = [
+        {name: 'Machamp', height: 1.6, type: 'fighting'},
+        {name: 'Primeape', height: 1, type: 'fighting'},
+        {name: 'Infernape', height: 1.2, type: ['fire', 'fighting']}
+    ];
+
+    function add(pokemon){
+        pokemonList.push(pokemon)
+    }
+
+    function getAll(pokemon){
+        return pokemonList;
+    }
+
+    return {
+        add,
+        getAll
+    }
+})();
 
 /*for (let i = 0; i < 3; i++) {
     document.write(pokemonList[i].name + '(height: ' + pokemonList[i].height + ')');
@@ -26,7 +47,7 @@ function myPokemon (pokemon) {
     document.write('<br>');
 }
 
-pokemonList.forEach(myPokemon);
+pokemonRepository.getAll().forEach(myPokemon);
 
 
 
