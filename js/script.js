73580dc4a17103c1e97fc81e958b16c0d24940da
Rjdorado96 <rjdorado96@outlook.com>
2022-11-10 20:21:02 -0600

    update to script.js file
    have added two additional function to GET external info from API.

--- js/script.js
+++ js/script.js
@@ -5,11 +5,9 @@
 ];*/
 
 let pokemonRepository = (function (){
-    let pokemonList = [
-        {name: 'Machamp', height: 1.6, type: 'fighting'},
-        {name: 'Primeape', height: 1, type: 'fighting'},
-        {name: 'Infernape', height: 1.2, type: ['fire', 'fighting']}
-    ];
+    let pokemonList = [];
+
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150'
 
     function add(pokemon){
         pokemonList.push(pokemon)
@@ -33,13 +31,46 @@ let pokemonRepository = (function (){
     }
 
     function showDetails(pokemon){
+        loadDetails(pokemon).then(function(){
         console.log(pokemon);
+        });
+    }
+
+    function loadList(){
+        return fetch(apiUrl).then(function(response){
+            return response.json();
+        }).then(function (json){
+            json.results.forEach(function(item){
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+            });
+        }).catch(function(e){
+            console.error(e);
+        })
+    }
+
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response){
+            return response.json();
+        }).then(function (details){
+            item.imageUrl = details.sprites.front_default;
+            item.height = details.height;
+            item.types = details.types;
+        }).catch(function (e){
+            console.error(e);
+        });
     }
 
     return {
         add,
         getAll,
-        addListItem
+        addListItem,
+        loadList,
+        loadDetails
     }
 })();
 
@@ -82,8 +113,10 @@ function myPokemon (pokemon) {
 
     document.write('<br>'); */
 }
+pokemonRepository.loadList().then(function(){
+    pokemonRepository.getAll().forEach(myPokemon);
+});
 
-pokemonRepository.getAll().forEach(myPokemon);
 
 
 
