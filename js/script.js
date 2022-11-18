33e7fe174abfd83f71b8ce34ddf24a02de89d7ed
Rjdorado96 <rjdorado96@outlook.com>
2022-11-18 15:17:22 -0600

    Update to script.js
    Fixed an error in the code in which i was calling a function within itself.

--- js/script.js
+++ js/script.js
@@ -68,61 +68,58 @@ let pokemonRepository = (function (){
     }
 })();
 
+
+  
+let modalContainer = document.querySelector('#modal-container');
+
 function showModal(pokemon) {
-  
-  let modalContainer = document.querySelector('#modal-container');
-  
-  function showModal(pokemon) {
-    modalContainer.innerHTML = '';
-    let modal = document.createElement('div');
-    modal.classList.add('modal');
+modalContainer.innerHTML = '';
+let modal = document.createElement('div');
+modal.classList.add('modal');
 
-    let closeButtonElement = document.createElement('button');
-    closeButtonElement.classList.add('modal-close');
-    closeButtonElement.innerText = 'Close';
-    closeButtonElement.addEventListener('click', hideModal);
+let closeButtonElement = document.createElement('button');
+closeButtonElement.classList.add('modal-close');
+closeButtonElement.innerText = 'Close';
+closeButtonElement.addEventListener('click', hideModal);
 
-    let titleElement = document.createElement('h1');
-    titleElement.innerText = pokemon.name;
+let titleElement = document.createElement('h1');
+titleElement.innerText = pokemon.name;
 
-    let contentElement = document.createElement('p');
-    contentElement.innerText = `Height: ${pokemon.height}`;
+let contentElement = document.createElement('p');
+contentElement.innerText = `Height: ${pokemon.height}`;
 
-    let imageElement = document.createElement('img')
-    imageElement.src = pokemon.imageUrl;
+let imageElement = document.createElement('img')
+imageElement.src = pokemon.imageUrl;
 
-    modal.appendChild(closeButtonElement);
-    modal.appendChild(titleElement);
-    modal.appendChild(contentElement);
-    modal.appendChild(imageElement);
-    modalContainer.appendChild(modal);
-    
-    modalContainer.classList.add('is-visible');
-  }
+modal.appendChild(closeButtonElement);
+modal.appendChild(titleElement);
+modal.appendChild(contentElement);
+modal.appendChild(imageElement);
+modalContainer.appendChild(modal);
 
-  function hideModal() {
-    modalContainer.classList.remove('is-visible');
-  }
-
-  window.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-      hideModal();  
-    }
-  });
-  
-  modalContainer.addEventListener('click', (e) => {
-    // Since this is also triggered when clicking INSIDE the modal
-    // We only want to close if the user clicks directly on the overlay
-    let target = e.target;
-    if (target === modalContainer) {
-      hideModal();
-    }
-  });
-
-  document.querySelector('#show-modal').addEventListener('click', () => {
-    showModal(pokemon);
-  });
+modalContainer.classList.add('is-visible');
 }
+
+function hideModal() {
+modalContainer.classList.remove('is-visible');
+}
+
+window.addEventListener('keydown', (e) => {
+if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+    hideModal();  
+}
+});
+
+modalContainer.addEventListener('click', (e) => {
+// Since this is also triggered when clicking INSIDE the modal
+// We only want to close if the user clicks directly on the overlay
+let target = e.target;
+if (target === modalContainer) {
+    hideModal();
+}
+});
+
+
 
 
 
